90ca2031973d7d67afffc6c689d4c28287283b60
ioannisk <john.karamanlakis@gmail.com>
2017-09-16 01:22:44 +0100

    cnn

--- model.py
+++ model.py
@@ -48,15 +48,15 @@ class CNN:
         g = tf.get_default_graph()
         x = g.get_tensor_by_name('input:0')
 
-        # for i in range(self.num_channels):
-        #     with tf.variable_scope('module_{}'.format(i)):
-        #         x = self.conv2d(x, 'conv1')
-        #         x = self.conv2d(x, 'conv2')
-        #         # x = self.pool(x)
+        for i in range(self.num_channels):
+            with tf.variable_scope('module_{}'.format(i)):
+                x = self.conv2d(x, 'conv1')
+                # x = self.conv2d(x, 'conv2')
+                # x = self.pool(x)
 
-        # isize = np.prod([d.value for d in x.shape[-3:]])
-        # x = tf.reshape(x, [-1, isize])
-        x = tf.reshape(x, [-1, 28*28])
+        isize = np.prod([d.value for d in x.shape[-3:]])
+        x = tf.reshape(x, [-1, isize])
+        # x = tf.reshape(x, [-1, 28*28])
         for i in range(self.num_fc):
             scope = 'fully_connected_{}'.format(i)
             x = self.fully_connected(x, self.num_hidden, scope)
@@ -102,7 +102,7 @@ def train(model, num_steps, batch_size, mnist):
 
 if __name__ == '__main__':
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-    model = CNN(num_modules=2,
+    model = CNN(num_modules=1,
                 num_fc=2,
                 ksize=3,
                 kstride=[1, 2, 2, 1],
